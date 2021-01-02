4b281a6e5cb555ac2503e2a7708d9a8e76697ded
AshuSrv <ashutoshmishra422@gmail.com>
2021-01-02 21:46:45 +0530

    Using Fetch for Api

--- app/routes/item.js
+++ app/routes/item.js
@@ -1,9 +1,12 @@
 import Route from '@ember/routing/route';
-import {products} from '../data/products'
+import { products } from '../data/products'
+
 export default class ItemRoute extends Route {
-    model(params) {
-        const { item_id } = params;
-        const product = products.find(( {id} ) => id === item_id);
+   async model(params) {
+       const { item_id } = params;
+       const response = await fetch('/api/items.json')
+       const {data} = await response.json()
+        const product = data.find(( {id} ) => id === item_id);
         console.log(product)
         return product;
     }
