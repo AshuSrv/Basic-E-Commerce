73daa7ab2adeeab3f9568fcb9642406c9d8b2b46
AshuSrv <ashutoshmishra422@gmail.com>
2021-01-04 22:42:02 +0530

    Unit Test in Service added

--- tests/unit/services/shopping-cart-test.js
+++ tests/unit/services/shopping-cart-test.js
@@ -9,4 +9,11 @@ module('Unit | Service | shopping-cart', function(hooks) {
     let service = this.owner.lookup('service:shopping-cart');
     assert.ok(service);
   });
+
+  test('item list Test', function(assert) {
+    let service = this.owner.lookup('service:shopping-cart');
+    assert.equal(service.itemList.length, 0, "Item list is Empty");
+    service.addItem({});
+    assert.equal(service.itemList.length, 1, 'Item list increased by 1');
+  });
 });
